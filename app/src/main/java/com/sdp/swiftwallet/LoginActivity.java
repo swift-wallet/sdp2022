e9ec15bc3fc494a000730af2bf62ba87abb31e4d
vfaltings <victor.faltings@epfl.ch>
2022-03-06 12:03:43 +0100

    Code refactor for the login method Refactored the code of the login method in LoginActivity by creating functions so that the function is more readable

--- app/src/main/java/com/sdp/swiftwallet/LoginActivity.java
+++ app/src/main/java/com/sdp/swiftwallet/LoginActivity.java
@@ -3,6 +3,7 @@ package com.sdp.swiftwallet;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +13,8 @@ import android.widget.TextView;
 
 import com.sdp.cryptowalletapp.R;
 
+import java.util.Locale;
+
 public class LoginActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE="com.sdp.swift-wallet.GREETING";
 
@@ -19,7 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     private static final String ADMIN_USERNAME = "admin";
     private static final String ADMIN_PASSWORD = "admin";
 
-    private TextView ATTEMPTS_TEXTVIEW;
+//    private TextView ATTEMPTS_TEXTVIEW;
     private static final int MAX_LOGIN_ATTEMPTS = 3;
     private int loginAttempts = 0;
 
@@ -27,65 +30,100 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        ATTEMPTS_TEXTVIEW = (TextView) findViewById(R.id.loginAttempts);
     }
 
     public void login(View view) {
-        Intent greetingIntent = new Intent(this, GreetingActivity.class);
-
         // Retrieve username and password from login screen
         EditText editText = (EditText) findViewById(R.id.loginUsername);
         String username = editText.getText().toString();
-
         editText = (EditText) findViewById(R.id.loginPassword);
         String password = editText.getText().toString();
 
         // Authenticate username and password
-        boolean successfulLogin = (
-                username.equals(ADMIN_USERNAME) && password.equals(ADMIN_PASSWORD)
-        );
+        boolean successfulLogin = authCredentials(username, password);
 
         if (successfulLogin) {
             // Launch next activity
-            greetingIntent.putExtra(GreetingActivity.EXTRA_MESSAGE, WELCOME_MESSAGE);
-            startActivity(greetingIntent);
+            startActivity(nextActivity(this));
         } else {
             // Check if over max attempts
             if (++loginAttempts >= MAX_LOGIN_ATTEMPTS) {
                 Intent mainIntent = new Intent(this, MainActivity.class);
-                new AlertDialog.Builder(this)
-                        .setTitle("Too many unsuccessful attempts")
-                        .setMessage("You have tried to login unsuccessfully too many times")
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                startActivity(mainIntent);
-                            }
-                        })
-                        .setCancelable(true)
-                        .create()
-                        .show();
+                tooManyAttemptsError(this, mainIntent).show();
+                return;
             }
 
             // Set attempts left text
-            String attemptsLeft = String.format(
-                    "You have %d attempts remaining",
-                    MAX_LOGIN_ATTEMPTS - loginAttempts
-            );
-            String test = "HELLO";
-            ATTEMPTS_TEXTVIEW.setText(test);
+//            String attemptsLeft = String.format(
+//                    Locale.US,
+//                    "You have %d attempts remaining",
+//                    MAX_LOGIN_ATTEMPTS - loginAttempts
+//            );
+//            String test = "HELLO";
+//            ATTEMPTS_TEXTVIEW.setText(test);
 
             // Display error message
-            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
-
-            alertBuilder.setTitle("Unsuccessful login...")
-                    .setMessage("Incorrect username or password")
-                    .setPositiveButton("OK", null)
-                    .setCancelable(true);
-
-            AlertDialog errorDialog = alertBuilder.create();
-            errorDialog.show();
+            incorrectCredentialsError(this).show();
         }
+    }
+
+    /**
+     * Prepares the intent for the next intent to launch when the login is successful
+     *
+     * @param context the context of the current Activity
+     * @return an Intent to launch the next activity
+     */
+    private Intent nextActivity(Context context) {
+        Intent nextActivity = new Intent(this, GreetingActivity.class);
+        nextActivity.putExtra(GreetingActivity.EXTRA_MESSAGE, WELCOME_MESSAGE);
+        return nextActivity;
+    }
+
+    /**
+     * Creates an AlertDialog to inform the user they used incorrect credentials
+     *
+     * @param context the context of the alert
+     * @return an AlertDialog informing the user their login attempt was unsuccessful
+     */
+    private AlertDialog incorrectCredentialsError(Context context) {
+        return new AlertDialog.Builder(context)
+                .setTitle("Unsuccessful login")
+                .setMessage("Incorrect username or password")
+                .setPositiveButton("OK", null)
+                .setCancelable(true)
+                .create();
+    }
+
+    /**
+     * Creates an AlertDialog to inform the user they have used up their login attempts
+     *
+     * @param context the context of the alert
+     * @param homeScreen the Intent of the Activity the user is sent to (i.e. the home screen)
+     * @return an AlertDialog informing the user they used up their login attempts which also
+     * starts another activity
+     */
+    private AlertDialog tooManyAttemptsError(Context context, Intent homeScreen) {
+        return new AlertDialog.Builder(context)
+                .setTitle("Too many unsuccessful attempts")
+                .setMessage("You have tried to login unsuccessfully too many times")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(homeScreen);
+                    }
+                })
+                .setCancelable(false)
+                .create();
+    }
+
+    /**
+     * Authenticates the user-provided credentials
+     *
+     * @param username the user-provided username
+     * @param password the user-provided password
+     * @return whether or not the credentials correspond to a valid account or not
+     */
+    private boolean authCredentials(String username, String password) {
+        return (username.equals(ADMIN_USERNAME) && password.equals(ADMIN_PASSWORD));
     }
 }
