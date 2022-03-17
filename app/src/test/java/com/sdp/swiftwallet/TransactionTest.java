95872e1a056c33f8c87c08ab17f9de0ef9b5664c
vfaltings <victor.faltings@epfl.ch>
2022-03-17 13:10:46 +0100

    Added test skeleton for Transaction class

---
+++ app/src/test/java/com/sdp/swiftwallet/TransactionTest.java
@@ -0,0 +1,56 @@
+package com.sdp.swiftwallet;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class TransactionTest {
+    @Test
+    public void constructorThrowsIAEOnNullCurr() {
+        
+    }
+
+    @Test
+    public void constructorThrowsIAEOnNullMyWallet() {
+
+    }
+
+    @Test
+    public void constructorThrowsIAEOnNullTheirWallet() {
+
+    }
+
+    @Test
+    public void constructorThrowsIAEOnDuplicateWallet() {
+
+    }
+
+    @Test
+    public void getAmountReturnsCorrectAmount() {
+
+    }
+
+    @Test
+    public void getConvertedAmountReturnsCorrectAmount() {
+
+    }
+
+    @Test
+    public void getSymbolReturnsCorrectSymbol() {
+
+    }
+
+    @Test
+    public void toStringReturnsCorrectStringOnNegativeAmount() {
+
+    }
+
+    @Test
+    public void toStringReturnsCorrectStringOnPositiveAmount() {
+
+    }
+}
