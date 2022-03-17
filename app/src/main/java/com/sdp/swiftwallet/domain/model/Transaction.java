8289a0e092e086c4575076ce01dc8a4ed9031551
vfaltings <victor.faltings@epfl.ch>
2022-03-17 13:11:34 +0100

    Added comments and argument checking to Transaction class Check for null arguments as well as duplicate wallets in the constructor of Transaction

--- app/src/main/java/com/sdp/swiftwallet/domain/model/Transaction.java
+++ app/src/main/java/com/sdp/swiftwallet/domain/model/Transaction.java
@@ -1,5 +1,7 @@
 package com.sdp.swiftwallet.domain.model;
 
+import androidx.annotation.NonNull;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -8,28 +10,62 @@ public class Transaction {
     private final double amount;
     private final Currency curr;
 
-    private final int transactionID; // transactionID to be stored in DB?
+    private final int transactionID;
 
     private final String myWallet;
     private final String theirWallet;
 
-    public Transaction(double amount, Currency curr, String myWallet, String theirWallet, int transactionID) {
+    /**
+     * Transaction object constructor
+     *
+     * @param amount the amount of the transaction
+     * @param curr with which currency the transaction was performed
+     * @param myWallet the ID of this user's wallet
+     * @param theirWallet the ID of the other wallet in the transaction
+     * @param transactionID the unique ID of the transaction
+     */
+    public Transaction(
+            double amount,
+            @NonNull Currency curr,
+            @NonNull String myWallet,
+            @NonNull String theirWallet,
+            int transactionID) {
+        if (curr == null || myWallet == null || theirWallet == null)
+            throw new IllegalArgumentException("Null arguments");
+
+        if (myWallet.equals(theirWallet))
+            throw new IllegalArgumentException("Wallets cannot be the same");
+
         this.amount = amount;
         this.curr = curr;
         this.transactionID = transactionID;
-
         this.myWallet = new String(myWallet);
         this.theirWallet = new String(theirWallet);
     }
 
+    /**
+     * Getter for the amount of a transaction
+     *
+     * @return the amount of this transaction
+     */
     public double getAmount() {
         return amount;
     }
 
+    /**
+     * Getter for the converted amount of a transaction
+     *
+     * @return the amount of this transaction, converted according to the currency
+     */
     public double getConvertedAmount() {
         return amount * curr.getValue();
     }
 
+    /**
+     * Getter for the symbol of this transaction
+     *
+     * @return the symbol of the currency of this transaction
+     */
     public String getSymbol() {
         return curr.getSymbol();
     }
