4d85faa32c717c66a1eae5ff123ce717c82fb30b
vfaltings <victor.faltings@epfl.ch>
2022-03-30 17:54:51 +0200

    added a button to stats fragment to store a random number on firestore

--- app/src/main/java/com/sdp/swiftwallet/presentation/main/fragments/StatsFragment.java
+++ app/src/main/java/com/sdp/swiftwallet/presentation/main/fragments/StatsFragment.java
@@ -3,16 +3,26 @@ package com.sdp.swiftwallet.presentation.main.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.sdp.cryptowalletapp.R;
 import com.sdp.swiftwallet.CryptoValuesActivity;
+import com.sdp.swiftwallet.domain.model.Transaction;
 import com.sdp.swiftwallet.presentation.transactions.TransactionActivity;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -34,6 +44,7 @@ public class StatsFragment extends Fragment {
         //This is temporary
         ((Button)getActivity().findViewById(R.id.cryptovalues_button)).setOnClickListener((v) -> startCryptovalues());
         ((Button)getActivity().findViewById(R.id.transaction_history_button)).setOnClickListener((v) -> startTransactionHistory());
+        ((Button)getActivity().findViewById(R.id.create_transaction_button)).setOnClickListener((v) -> createDummyTransaction());
     }
 
     private void startCryptovalues(){
@@ -43,5 +54,23 @@ public class StatsFragment extends Fragment {
     private void startTransactionHistory(){
         Intent intent = new Intent(getContext(), TransactionActivity.class);
         startActivity(intent);
+    }
+
+    private void createDummyTransaction() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Random rand = new Random();
+
+        Map<String, String> map = new HashMap<>();
+        map.put("number", Integer.toString(rand.nextInt()));
+
+        db.collection("transactions").document().set(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getActivity().getApplicationContext(), "Data saved", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
