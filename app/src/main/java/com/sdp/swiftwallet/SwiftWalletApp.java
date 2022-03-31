a167aca56285d5d14491c1486e2e53b5dbd91cba
0xdieman <diego.boros@epfl.ch>
2022-03-31 13:18:59 +0200

    Add hilt to app

--- app/src/main/java/com/sdp/swiftwallet/SwiftWalletApp.java
+++ app/src/main/java/com/sdp/swiftwallet/SwiftWalletApp.java
@@ -2,6 +2,9 @@ package com.sdp.swiftwallet;
 
 import android.app.Application;
 
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
 public class SwiftWalletApp extends Application {
 
 }
