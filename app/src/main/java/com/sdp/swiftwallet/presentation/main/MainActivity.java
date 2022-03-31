dfaa499adf320aa6a964da516d0d93d4dcc599f5
Jérôme <jerome.ceccaldi@epfl.ch>
2022-03-31 23:22:18 +0200

    Greatly simplified navigation bar for main
    - Removed messy bottom navigation setup in MainActivity
    - Used navigation graph to setup bottom navigation

--- app/src/main/java/com/sdp/swiftwallet/presentation/main/MainActivity.java
+++ app/src/main/java/com/sdp/swiftwallet/presentation/main/MainActivity.java
@@ -7,6 +7,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -40,68 +44,74 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ((BottomNavigationView)findViewById(R.id.bottom_bar)).setOnItemSelectedListener(navigationItemSelectedListener);
-        //Creating all the navigation fragments
-        homeFragment = new HomeFragment();
-        statsFragment = new StatsFragment();
-        messageFragment = new MessageFragment();
-        paymentFragment = new PaymentFragment();
-        profileFragment = new ProfileFragment();
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.main_nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.mainBottomNavView);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
-        activeFragment = homeFragment;
-
-        fragmentManager = getSupportFragmentManager();
-
-        fragmentManager
-                .beginTransaction()
-                .add(R.id.fragment_container,homeFragment, HomeFragment.class.getName())
-                .add(R.id.fragment_container,statsFragment, StatsFragment.class.getName())
-                .add(R.id.fragment_container,messageFragment, MessageFragment.class.getName())
-                .add(R.id.fragment_container,paymentFragment, PaymentFragment.class.getName())
-                .add(R.id.fragment_container,profileFragment, ProfileFragment.class.getName())
-                .hide(statsFragment).hide(messageFragment).hide(paymentFragment).hide(profileFragment)
-                .setReorderingAllowed(true)
-                .commit();
+//        ((BottomNavigationView)findViewById(R.id.mainBottomNavView)).setOnItemSelectedListener(navigationItemSelectedListener);
+//        //Creating all the navigation fragments
+//        homeFragment = new HomeFragment();
+//        statsFragment = new StatsFragment();
+//        messageFragment = new MessageFragment();
+//        paymentFragment = new PaymentFragment();
+//        profileFragment = new ProfileFragment();
+//
+//        activeFragment = homeFragment;
+//
+//        fragmentManager = getSupportFragmentManager();
+//
+//        fragmentManager
+//                .beginTransaction()
+//                .add(R.id.fragment_container,homeFragment, HomeFragment.class.getName())
+//                .add(R.id.fragment_container,statsFragment, StatsFragment.class.getName())
+//                .add(R.id.fragment_container,messageFragment, MessageFragment.class.getName())
+//                .add(R.id.fragment_container,paymentFragment, PaymentFragment.class.getName())
+//                .add(R.id.fragment_container,profileFragment, ProfileFragment.class.getName())
+//                .hide(statsFragment).hide(messageFragment).hide(paymentFragment).hide(profileFragment)
+//                .setReorderingAllowed(true)
+//                .commit();
     }
 
-    /**
-     * Setup bottom navigation listener on the menu
-     */
-    private final BottomNavigationView.OnItemSelectedListener navigationItemSelectedListener =
-
-        //Sets up on click listener to fragments of the home activity
-        new NavigationBarView.OnItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.bar_home:
-                    selectedFragment = homeFragment;
-                    break;
-                case R.id.bar_stats:
-                    selectedFragment = statsFragment;
-                    break;
-                case R.id.bar_payment:
-                    selectedFragment = paymentFragment;
-                    break;
-                case R.id.bar_message:
-                    selectedFragment = messageFragment;
-                    break;
-                case R.id.bar_profile:
-                    selectedFragment = profileFragment;
-                    break;
-            }
-
-            if (selectedFragment != null) {
-                fragmentManager
-                        .beginTransaction()
-                        .hide(activeFragment)
-                        .show(selectedFragment)
-                        .setReorderingAllowed(true)
-                        .commit();
-                activeFragment = selectedFragment;
-                }
-                    return true;
-        }
-        };
+//    /**
+//     * Setup bottom navigation listener on the menu
+//     */
+//    private final BottomNavigationView.OnItemSelectedListener navigationItemSelectedListener =
+//
+//        //Sets up on click listener to fragments of the home activity
+//        new NavigationBarView.OnItemSelectedListener() {
+//        @Override
+//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//            switch (item.getItemId()) {
+//                case R.id.mainNavHomeItem:
+//                    selectedFragment = homeFragment;
+//                    break;
+//                case R.id.mainNavStatsItem:
+//                    selectedFragment = statsFragment;
+//                    break;
+//                case R.id.mainNavPaymentItem:
+//                    selectedFragment = paymentFragment;
+//                    break;
+//                case R.id.mainNavMessageItem:
+//                    selectedFragment = messageFragment;
+//                    break;
+//                case R.id.mainNavProfileItem:
+//                    selectedFragment = profileFragment;
+//                    break;
+//            }
+//
+//            if (selectedFragment != null) {
+//                fragmentManager
+//                        .beginTransaction()
+//                        .hide(activeFragment)
+//                        .show(selectedFragment)
+//                        .setReorderingAllowed(true)
+//                        .commit();
+//                activeFragment = selectedFragment;
+//                }
+//                    return true;
+//        }
+//        };
 
 }
