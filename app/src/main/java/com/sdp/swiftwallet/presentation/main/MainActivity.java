fdffdcf2efb4989964c497cf07572374f82f5ebd
Jérôme <jerome.ceccaldi@epfl.ch>
2022-04-01 00:42:41 +0200

    Fixed main layout and home fragment
    - Use of constraintLayout to improve layout quality
    - Refactoring in String xml
    - Cleaning in mainActivity

--- app/src/main/java/com/sdp/swiftwallet/presentation/main/MainActivity.java
+++ app/src/main/java/com/sdp/swiftwallet/presentation/main/MainActivity.java
@@ -29,19 +29,6 @@ import dagger.hilt.android.AndroidEntryPoint;
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
 
-    private FragmentManager fragmentManager;
-    Fragment selectedFragment = null;
-    HomeFragment homeFragment;
-    StatsFragment statsFragment;
-    PaymentFragment paymentFragment;
-    MessageFragment messageFragment;
-    ProfileFragment profileFragment;
-
-    Fragment activeFragment;
-
-    /**
-     * Methods that are called on creation
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,69 +39,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = navHostFragment.getNavController();
         BottomNavigationView bottomNavigationView = findViewById(R.id.mainBottomNavView);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
-
-//        ((BottomNavigationView)findViewById(R.id.mainBottomNavView)).setOnItemSelectedListener(navigationItemSelectedListener);
-//        //Creating all the navigation fragments
-//        homeFragment = new HomeFragment();
-//        statsFragment = new StatsFragment();
-//        messageFragment = new MessageFragment();
-//        paymentFragment = new PaymentFragment();
-//        profileFragment = new ProfileFragment();
-//
-//        activeFragment = homeFragment;
-//
-//        fragmentManager = getSupportFragmentManager();
-//
-//        fragmentManager
-//                .beginTransaction()
-//                .add(R.id.fragment_container,homeFragment, HomeFragment.class.getName())
-//                .add(R.id.fragment_container,statsFragment, StatsFragment.class.getName())
-//                .add(R.id.fragment_container,messageFragment, MessageFragment.class.getName())
-//                .add(R.id.fragment_container,paymentFragment, PaymentFragment.class.getName())
-//                .add(R.id.fragment_container,profileFragment, ProfileFragment.class.getName())
-//                .hide(statsFragment).hide(messageFragment).hide(paymentFragment).hide(profileFragment)
-//                .setReorderingAllowed(true)
-//                .commit();
     }
-
-//    /**
-//     * Setup bottom navigation listener on the menu
-//     */
-//    private final BottomNavigationView.OnItemSelectedListener navigationItemSelectedListener =
-//
-//        //Sets up on click listener to fragments of the home activity
-//        new NavigationBarView.OnItemSelectedListener() {
-//        @Override
-//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//            switch (item.getItemId()) {
-//                case R.id.mainNavHomeItem:
-//                    selectedFragment = homeFragment;
-//                    break;
-//                case R.id.mainNavStatsItem:
-//                    selectedFragment = statsFragment;
-//                    break;
-//                case R.id.mainNavPaymentItem:
-//                    selectedFragment = paymentFragment;
-//                    break;
-//                case R.id.mainNavMessageItem:
-//                    selectedFragment = messageFragment;
-//                    break;
-//                case R.id.mainNavProfileItem:
-//                    selectedFragment = profileFragment;
-//                    break;
-//            }
-//
-//            if (selectedFragment != null) {
-//                fragmentManager
-//                        .beginTransaction()
-//                        .hide(activeFragment)
-//                        .show(selectedFragment)
-//                        .setReorderingAllowed(true)
-//                        .commit();
-//                activeFragment = selectedFragment;
-//                }
-//                    return true;
-//        }
-//        };
 
 }
