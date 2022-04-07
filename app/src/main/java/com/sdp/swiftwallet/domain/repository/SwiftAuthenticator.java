c3d77dbbe4e46679ca215820ca6ed1fd53168361
vfaltings <victor.faltings@epfl.ch>
2022-04-07 02:00:23 +0200

    Added SwiftAuthenticator interface

---
+++ app/src/main/java/com/sdp/swiftwallet/domain/repository/SwiftAuthenticator.java
@@ -0,0 +1,34 @@
+package com.sdp.swiftwallet.domain.repository;
+
+import com.sdp.swiftwallet.domain.model.User;
+
+import java.util.Optional;
+
+public interface SwiftAuthenticator {
+
+    enum Result {
+        SUCCESS, NULL_EMAIL, NULL_PASSWORD, ERROR
+    }
+
+    class Credentials {
+        private final String email;
+        private final String password;
+
+        public Credentials(String email, String password) {
+            this.email = email;
+            this.password = password;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+    }
+
+    Result signIn(Optional<Credentials> credentials, Runnable success, Runnable failure);
+
+    Optional<User> getUser();
+}
