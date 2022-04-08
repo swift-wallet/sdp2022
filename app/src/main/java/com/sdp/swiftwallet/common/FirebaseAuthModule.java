cc127347bc1e83394b72ef0ffd99e96ed4874350
vfaltings <victor.faltings@epfl.ch>
2022-04-08 14:54:21 +0200

    changed firebaseauthmodule to singleton

--- app/src/main/java/com/sdp/swiftwallet/common/FirebaseAuthModule.java
+++ app/src/main/java/com/sdp/swiftwallet/common/FirebaseAuthModule.java
@@ -6,9 +6,10 @@ import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.android.components.ActivityComponent;
+import dagger.hilt.components.SingletonComponent;
 
 @Module
-@InstallIn(ActivityComponent.class)
+@InstallIn(SingletonComponent.class)
 public class FirebaseAuthModule {
     @Provides
     public FirebaseAuth firebaseAuthProvider(){
