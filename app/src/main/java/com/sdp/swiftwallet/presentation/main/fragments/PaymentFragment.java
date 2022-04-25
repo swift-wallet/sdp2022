0fbf3e557edcfec9b2d052a6cc0e2abe5817dcff
0xdieman <diego.boros@epfl.ch>
2022-04-25 10:11:07 +0200

    Add SeekBar and tests

--- app/src/main/java/com/sdp/swiftwallet/presentation/main/fragments/PaymentFragment.java
+++ app/src/main/java/com/sdp/swiftwallet/presentation/main/fragments/PaymentFragment.java
@@ -1,11 +1,15 @@
 package com.sdp.swiftwallet.presentation.main.fragments;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -14,7 +18,6 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.sdp.cryptowalletapp.R;
-import com.sdp.swiftwallet.SwiftWalletApp;
 import com.sdp.swiftwallet.data.repository.Web3Requests;
 import com.sdp.swiftwallet.di.WalletProvider;
 import com.sdp.swiftwallet.domain.model.QRCodeScanner;
@@ -32,6 +35,10 @@ public class PaymentFragment extends Fragment {
     private TextView fromAddress;
     private TextView fromBalance;
     private TextView toAddress;
+    private EditText sendAmount;
+    private SeekBar seekBar;
+
+    private boolean isSeeking = false;
 
     private final OnFromAddressSelected onFromAddressSelected = new OnFromAddressSelected();
     QRCodeScanner qrCodeScanner = new QRCodeScanner(this::setToSelectedAddress, this);
@@ -67,7 +74,15 @@ public class PaymentFragment extends Fragment {
         fromBalance = view.findViewById(R.id.send_from_balance);
         toAddress = view.findViewById(R.id.send_to_address);
 
-        // Initialize the address spinner
+        // Seek bar and send amount
+        sendAmount = view.findViewById(R.id.send_amount);
+        seekBar = view.findViewById(R.id.send_amount_seekBar);
+        // When amount changes, adapt the seek bar's progress
+        sendAmount.addTextChangedListener(new AmountWatcher());
+        // When seekbar is touched by user, adapt the amount
+        seekBar.setOnSeekBarChangeListener(new SeekBarWatcher());
+
+        // Initializes the address spinner
         Spinner walletsSpinner = view.findViewById(R.id.send_from_spinner);
         walletsSpinner.setAdapter(arrayAdapter);
         walletsSpinner.setOnItemSelectedListener(onFromAddressSelected);
@@ -78,6 +93,10 @@ public class PaymentFragment extends Fragment {
     private void setToSelectedAddress(String to){
         toAddress.setText(to);
     }
+
+    /**
+     * Those private classes are implementation needed for some view listeners of this fragment
+     */
 
     private class OnFromAddressSelected implements AdapterView.OnItemSelectedListener{
         @Override
@@ -91,6 +110,48 @@ public class PaymentFragment extends Fragment {
         }
         @Override
         public void onNothingSelected(AdapterView<?> adapterView) {
+        }
+    }
+
+    private class SeekBarWatcher implements SeekBar.OnSeekBarChangeListener {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            if(isSeeking){
+                float amount = seekBar.getProgress() * Float.parseFloat(fromBalance.getText().toString()) / 100;
+                sendAmount.setText(Float.toString(amount));
+            }
+        }
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+            isSeeking = true;
+        }
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            isSeeking = false;
+        }
+    }
+
+    private class AmountWatcher implements TextWatcher {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+        @Override
+        public void afterTextChanged(Editable editable) {
+            if(sendAmount.getText().toString().equals("")) return;
+            float sendValue = Float.parseFloat(sendAmount.getText().toString());
+            float maxValue = Float.parseFloat(fromBalance.getText().toString());
+            float progressPercentage;
+            if(sendValue > maxValue){
+                progressPercentage = 100;
+                sendAmount.setText(Float.toString(maxValue));
+            }else if(sendValue < 0){
+                progressPercentage = 0;
+                sendAmount.setText(0);
+            }else{
+                progressPercentage = (sendValue / maxValue)*100;
+            }
+            seekBar.setProgress((int)progressPercentage);
         }
     }
 }
