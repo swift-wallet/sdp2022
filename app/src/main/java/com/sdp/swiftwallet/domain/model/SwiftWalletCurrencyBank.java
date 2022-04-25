10af519f8812b0da26c8f8148e7fc3ce0fbf2e7c
vfaltings <victor.faltings@epfl.ch>
2022-04-25 13:13:37 +0200

    optimized imports

--- app/src/main/java/com/sdp/swiftwallet/domain/model/SwiftWalletCurrencyBank.java
+++ app/src/main/java/com/sdp/swiftwallet/domain/model/SwiftWalletCurrencyBank.java
@@ -1,22 +1,7 @@
 package com.sdp.swiftwallet.domain.model;
 
-import android.content.Context;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import dagger.hilt.android.qualifiers.ApplicationContext;
 
 public class SwiftWalletCurrencyBank implements CurrencyBank {
     private final Map<String, Currency> currencyMap;
