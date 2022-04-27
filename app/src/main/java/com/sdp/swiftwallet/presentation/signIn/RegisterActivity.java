fc48547c5272d0ca7f3934d8b7485459d3289d3d
Jérôme <jerome.ceccaldi@epfl.ch>
2022-04-27 17:11:19 +0200

    Interface changes in register activity
    - Added colors and shape for interface theme
    - Register button now display loading icon when waiting for firebase
    - Added confirm password input to avoid user's error
    - Added image configuration for user profile (not implemented yet)
    - Use of bindings instead of ViewId
    - New helper function to display Toast easily

--- app/src/main/java/com/sdp/swiftwallet/presentation/signIn/RegisterActivity.java
+++ app/src/main/java/com/sdp/swiftwallet/presentation/signIn/RegisterActivity.java
@@ -3,39 +3,35 @@ package com.sdp.swiftwallet.presentation.signIn;
 import static com.sdp.swiftwallet.common.HelperFunctions.checkEmail;
 import static com.sdp.swiftwallet.common.HelperFunctions.checkPassword;
 import static com.sdp.swiftwallet.common.HelperFunctions.checkUsername;
+import static com.sdp.swiftwallet.common.HelperFunctions.displayToast;
 import static com.sdp.swiftwallet.domain.repository.SwiftAuthenticator.LoginMethod.BASIC;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.test.espresso.idling.CountingIdlingResource;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.sdp.cryptowalletapp.R;
+import com.sdp.cryptowalletapp.databinding.ActivityRegisterBinding;
+import com.sdp.swiftwallet.common.Constants;
 import com.sdp.swiftwallet.common.FirebaseUtil;
 import com.sdp.swiftwallet.domain.model.User;
 
 public class RegisterActivity extends AppCompatActivity {
     private static final String EMAIL_REGISTER_TAG = "EMAIL_REGISTER_TAG";
 
+    private ActivityRegisterBinding binding;
+
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
-    private EditText registerUsernameEt, registerEmailEt, registerPasswordEt;
 
     // Used for debugging purpose and testing
     private CountingIdlingResource mIdlingResource;
@@ -43,47 +39,57 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         // Init auth client and db client
         mAuth = FirebaseUtil.getAuth();
         db = FirebaseUtil.getFirestore();
 
-        // Find EditText view and registerBtn
-        registerUsernameEt = findViewById(R.id.registerUsernameEt);
-        registerEmailEt = findViewById(R.id.registerEmailEt);
-        registerPasswordEt = findViewById(R.id.registerPasswordEt);
-        Button registerBtn = findViewById(R.id.registerBtn);
-
         // Init counting resource for async call in test
         mIdlingResource = new CountingIdlingResource("Register Calls");
 
-        // Init listeners
-        Button goBack = findViewById(R.id.goBackRegister);
-        goBack.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                RegisterActivity.this.startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-            }
-        });
+        setListeners();
+    }
 
-        registerBtn.setOnClickListener(v -> registerUser());
+    /**
+     * Set all listeners from registerActivity
+     */
+    private void setListeners() {
+        binding.registerBtn.setOnClickListener(v -> registerUser());
+    }
+
+    /**
+     * Enable loading icon for register button
+     * @param isLoading flag to enable or disable loading
+     */
+    private void loading(Boolean isLoading) {
+        if (isLoading) {
+            binding.registerBtn.setVisibility(View.INVISIBLE);
+            binding.registerBtnProgressBar.setVisibility(View.VISIBLE);
+        } else {
+            binding.registerBtnProgressBar.setVisibility(View.INVISIBLE);
+            binding.registerBtn.setVisibility(View.VISIBLE);
+        }
     }
 
     /**
      * Check for registration info validity and create user on client auth
      */
     private void registerUser() {
-        String username = registerUsernameEt.getText().toString().trim();
-        String email = registerEmailEt.getText().toString().trim();
-        String password = registerPasswordEt.getText().toString().trim();
+        String username = binding.registerInputUsername.getText().toString().trim();
+        String email = binding.registerInputEmail.getText().toString().trim();
+        String password = binding.registerInputPassword.getText().toString().trim();
+        String confirmPassword = binding.registerInputConfirmPassword.getText().toString().trim();
 
         // Check validity of inputs
-        if (!checkUsername(username, registerUsernameEt)) return;
-        if (!checkEmail(email, registerEmailEt)) return;
-        if (!checkPassword(password, registerPasswordEt)) return;
+        if (!checkUsername(username, binding.registerInputUsername)) return;
+        if (!checkEmail(email, binding.registerInputEmail)) return;
+        if (!checkPassword(password, confirmPassword, binding.registerInputPassword)) return;
 
         // Increment counter before creating user
         mIdlingResource.increment();
+        loading(true);
         createUserWithEmailAndPassword(username, email, password);
     }
 
@@ -101,9 +107,11 @@ public class RegisterActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             User user = new User(username, BASIC);
+
+                            // Dev feedback only (intermediate step)
                             Log.d(EMAIL_REGISTER_TAG, "onComplete: User created for Auth");
-                            // If user successfuly created the account,
-                            // Increment before adding to db and decrement because user is created
+
+                            // Increment before database step begins; decrement to end auth step
                             mIdlingResource.increment();
                             registerUserToDatabase(user);
                             mIdlingResource.decrement();
@@ -111,7 +119,9 @@ public class RegisterActivity extends AppCompatActivity {
                         else {
                             // If user failed to create an account, decrement counter
                             mIdlingResource.decrement();
-                            Toast.makeText(RegisterActivity.this, "User failed to register", Toast.LENGTH_LONG).show();
+
+                            // User and Dev feedback
+                            displayToast(getApplicationContext(), "User failed to register");
                             Log.w(EMAIL_REGISTER_TAG, "Error from task", task.getException());
                         }
                     }
@@ -123,27 +133,27 @@ public class RegisterActivity extends AppCompatActivity {
      * @param user the user to add to the database
      */
     private void registerUserToDatabase(User user) {
-        db.collection("users")
+        db.collection(Constants.KEY_COLLECTION_USERS)
                 .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Toast.makeText(RegisterActivity.this, "User successfully registered", Toast.LENGTH_LONG).show();
-                        Log.d(EMAIL_REGISTER_TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                .addOnSuccessListener(documentReference -> {
+                    loading(false);
 
-                        // Decrement counter if user successfully added to db
-                        mIdlingResource.decrement();
-                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-                    }
+                    // User and Dev feedback
+                    displayToast(getApplicationContext(), "User successfully registered");
+                    Log.d(EMAIL_REGISTER_TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+
+                    // Decrement counter if user successfully added to db
+                    mIdlingResource.decrement();
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
                 })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // Decrement counter if user failed to add to db
-                        mIdlingResource.decrement();
-                        Toast.makeText(RegisterActivity.this, "User failed to register", Toast.LENGTH_LONG).show();
-                        Log.w(EMAIL_REGISTER_TAG, "Error adding document", e);
-                    }
+                .addOnFailureListener(e -> {
+                    loading(false);
+                    // Decrement counter if user failed to add to db
+                    mIdlingResource.decrement();
+
+                    // User and Dev feedback
+                    displayToast(getApplicationContext(), "User failed to register");
+                    Log.w(EMAIL_REGISTER_TAG, "Error adding document", e);
                 });
     }
 
