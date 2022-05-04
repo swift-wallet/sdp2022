cbf991a551a18cf0713cc5881a574f976524dbb6
Jérôme <jerome.ceccaldi@epfl.ch>
2022-05-04 20:13:03 +0200

    Resolved last conflict

--- app/src/main/java/com/sdp/swiftwallet/presentation/signIn/LoginActivity.java
+++ app/src/main/java/com/sdp/swiftwallet/presentation/signIn/LoginActivity.java
@@ -8,11 +8,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-<<<<<<< HEAD
-import android.widget.Toast;
-
-=======
->>>>>>> main
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
