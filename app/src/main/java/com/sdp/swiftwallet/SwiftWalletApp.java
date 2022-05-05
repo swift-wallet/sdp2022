96cefa0fe432cbb7b69adbbbee705d534ceb11d0
Jérôme <jerome.ceccaldi@epfl.ch>
2022-05-06 00:04:19 +0200

    Added links in doc

--- app/src/main/java/com/sdp/swiftwallet/SwiftWalletApp.java
+++ app/src/main/java/com/sdp/swiftwallet/SwiftWalletApp.java
@@ -4,7 +4,11 @@ import dagger.hilt.android.HiltAndroidApp;
 
 /**
  * This class is only required for the @HiltAndroidApp tag to make injection work
- * If you want to add global attributes/methods for the app, see BaseApp
+ * If you want to add global attributes/methods for the app, see BaseApp.
+ *
+ * The tag cannot be simply put in BaseApp as it would break hilt,
+ * See: https://stackoverflow.com/questions/68850381/android-tests-failing-when-using-hilt-caused-by-classcastexception
+ * See: https://github.com/google/dagger/issues/2033
  */
 @HiltAndroidApp
 public class SwiftWalletApp extends BaseApp { }
